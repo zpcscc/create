1f89f5100aaf33f9610b179bee188d006f24b9aa
dxsixpc <1071667708@qq.com>
2023-03-17 21:25:41 +0800

    🔥update: 更新依赖，调整release、logo

--- template/dumi-react/.dumirc.ts
+++ template/dumi-react/.dumirc.ts
@@ -6,9 +6,6 @@ export default defineConfig({
   outputPath: 'docs-dist',
   themeConfig: {
     name,
-    socialLinks: {
-      github: 'https://github.com/dxsixpc/create',
-    },
   },
   base: `/${name}/`,
   publicPath: `/${name}/`,
